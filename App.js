2f0548a78ae03a4256e95f915ac30a1f3b2051d7
kjh-busan <picsmash7@gmail.com>
2023-02-26 00:11:38 +0900

    feat: init

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
-import { View, SafeAreaView } from 'react-native';
-import TodoHeader from './src/components/TodoHeader';
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import Header from './Header'
+import Body from './Body'
 
-export default function App() {
-  const RootApp = () => {
+export default class App extends React.Component {
+  render() {
     return (
-      <SafeAreaView>
-        <TodoHeader />
-      </SafeAreaView>
-    )
+      <View style={styles.container}>
+        <Text style={styles.title}>Todo App</Text>
+        <Header />
+        <Body />
+      </View>
+    );
   }
-  
-  return (
-    <View>
-      <RootApp />
-    </View>
-  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingTop: 50,
+    backgroundColor: "#EEE",
+  },
+  title: {
+    fontWeight: "800",
+    fontSize: 30, 
+    marginLeft: 20,
+    marginBottom: 20,
+  }
+});
---
+++ Body.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+class Body extends Component {
+    state = [
+        {
+            text: '할일1',
+            completed: false,
+        },
+        {
+            text: '할일2',
+            completed: true,
+        },
+        {
+            text: '할일3',
+            completed: false,
+        },
+    ]
+
+    render() {
+        return (
+            <View style={styles.container}>
+                {
+                    this.state.map(data => (
+                        <View style={styles.todo}>
+                            <View style={styles.todoText}>
+                                <TouchableOpacity style={styles.todoCheckbox}>
+                                    {
+                                        data.completed
+                                        ? <MaterialCommunityIcons size={20} name='checkbox-marked-circle-outline' />
+                                        : <MaterialCommunityIcons size={20} name='checkbox-blank-circle-outline' />
+                                    }
+                                </TouchableOpacity>
+                                <Text>{data.text}</Text>
+                            </View>
+                            <TouchableOpacity>
+                                <MaterialCommunityIcons style={styles.todoDelBtn} size={30} name='delete-outline' />
+                            </TouchableOpacity>
+                        </View>
+                    ))
+                }
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginVertical: 5,
+        marginHorizontal: 20,
+        padding: 10,
+        backgroundColor: "#FFF",
+        borderRadius: 10,
+    },
+    todo: {
+        flexDirection: 'row',
+        alignItems: "center",
+        justifyContent: 'space-between',
+        height: 50,
+        borderBottomColor: "#bbb",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+    todoCheckbox: {
+        marginRight: 5,
+    },
+    todoText: {
+        flexDirection: 'row',
+    },
+    todoDelBtn: {
+        color: '#777'
+    }
+});
+
+export default Body;
